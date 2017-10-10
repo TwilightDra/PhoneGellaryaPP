d70fe4bafc6a40e4293d7833f5c52c9611627c96
cheyu920926 <cheyu0926@gmail.com>
2017-10-10 02:14:38 -0700

    N/A

---
+++ app/src/main/java/com/example/td/phonegellary/cameraTest.java
@@ -0,0 +1,13 @@
+package com.example.td.phonegellary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cameraTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_test);
+    }
+}
--- app/src/main/java/com/example/td/phonegellary/filesystest.java
+++ app/src/main/java/com/example/td/phonegellary/filesystest.java
@@ -1,22 +1,43 @@
 package com.example.td.phonegellary;
 
+import java.io.File;
+import java.io.ByteArrayOutputStream;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
+import android.os.Environment;
 import android.database.sqlite.SQLiteDatabase;
+import android.view.View;
 import android.widget.Toast;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.provider.MediaStore;
 import android.database.Cursor;
 
 public class filesystest extends AppCompatActivity {
 
     private int READ_STORAGE_PERMISSION_CODE = 23;
     private int WRITE_STORAGE_PERMISSION_CODE = 24;
+    final int TAKE_PICTURE=1;
+    private  Button btnTakePic=null;
+    private ImageView imageView=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filesystest);
+        btnTakePic=(Button) findViewById(R.id.btn_TakePic);
+        btnTakePic.setOnClickListener(onClickListener);
+        imageView=(ImageView) findViewById(R.id.imageView16);
+
+
         String dbName = "tempDB.db";
         SQLiteDatabase db = openOrCreateDatabase(dbName, 0, null);
 
@@ -80,6 +101,30 @@ public class filesystest extends AppCompatActivity {
         }
         catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private final View.OnClickListener onClickListener= new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            Intent intent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            ContentValues contentValues=new ContentValues(1);
+            contentValues.put(MediaStore.Images.Media.DATA, "temp.jpg");
+            Uri fileUri=getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+            //startActivityForResult(intent, TAKE_PICTURE);
+            startActivity(intent);
+        }
+    };
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode==TAKE_PICTURE){
+            if(resultCode==RESULT_OK){
+                Bundle bundle=data.getExtras();
+                Bitmap photo=(Bitmap)bundle.get("data");
+                imageView.setImageBitmap(photo);
+            }
+
         }
     }
 }
